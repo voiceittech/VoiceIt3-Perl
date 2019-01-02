e7c69ba661318954f418c32bca71d9afe5221ccc
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-01-02 12:00:36 -0800

    try to fix CPAN RELEASEPATCH

--- cpan.sh
+++ cpan.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 commit=$(git log -1 --pretty=%B | head -n 1)
-curl -s https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases/latest | grep '"tag_name":' |sed -E ' s/.*"([^"]+)".*/\1/'
 version=$(echo $(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases/latest | grep '"tag_name":' |sed -E ' s/.*"([^"]+)".*/\1/') | tr "." "\n")
 set -- $version
 major=$1
 minor=$2
 patch=$3
+echo 'old version='$major'.'$minor'.'$patch
 
 if [[ $commit = *"RELEASE"* ]];
 then
@@ -30,18 +30,20 @@ then
 
   version=$major'.'$minor'.'$patch
   echo $version
-  # mkdir -p CPAN/lib/voiceIt
-  # mv voiceIt2.pm CPAN/lib/voiceIt
-  # cd CPAN
-  # h2xs -AX --skip-exporter --use-new-tests voiceIt::voiceIt2 -v $version
-  # cd voiceIt-voiceIt2
-  # perl Makefile.PL
-  # make dist
+  echo 'new version='$major'.'$minor'.'$patch
+  mkdir -p CPAN/lib/voiceIt
+  mv voiceIt2.pm CPAN/lib/voiceIt
+  cd CPAN
+  h2xs -AX --skip-exporter --use-new-tests voiceIt::voiceIt2 -v $version
+  cd voiceIt-voiceIt2
+  perl Makefile.PL
+  make dist
 
-  # echo "ls"
-  # ls
-  # cpan install CPAN-Uploader-0.103013::cpan-upload
-  # which cpan-upload
-  # cpan-upload -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
-  # echo "PERL PAUSE SUCCESS"
+  echo "ls"
+  ls
+  cpan install CPAN-Uploader-0.103013::cpan-upload
+  which cpan-upload
+  # curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases
+  cpan-upload -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
+  echo "PERL PAUSE SUCCESS"
 fi
