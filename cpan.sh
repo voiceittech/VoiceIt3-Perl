954009bee22c0916cd82b8808b3b742084f54f52
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-01-02 12:08:33 -0800

    try to fix CPAN RELEASEPATCH

--- cpan.sh
+++ cpan.sh
@@ -39,7 +39,7 @@ then
   make dist
 
   # cpan install CPAN-Uploader-0.103013::cpan-upload
-  cpan CPAN::Uploader::cpan-upload
+  cpan CPAN::Uploader
   # curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases
   cpan-upload -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
 fi
