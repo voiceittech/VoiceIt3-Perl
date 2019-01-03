d89efa31fd583738c44844952866696105735a27
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-01-03 14:10:39 -0600

    revert testing, RELEASEMINOR

--- cpan.sh
+++ cpan.sh
@@ -29,17 +29,17 @@ then
   version=$major'.'$minor
   echo 'new version='$major'.'$minor
 
-  # mkdir -p CPAN/lib/voiceIt
-  # cp voiceIt2.pm CPAN/lib/voiceIt
-  # cd CPAN
-  # h2xs -AX --skip-exporter --use-new-tests voiceIt::voiceIt2 -v $version
-  # cd voiceIt-voiceIt2
-  # perl Makefile.PL
-  # make dist
-#
-  # cpan CPAN::Uploader
-  # curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases
-  # echo "ls"
-  # ls
-  # cpan-upload --ignore-errors -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD -d 'voiceIt-voiceIt2-'$version 'voiceIt-voiceIt2-'$version'.tar.gz'
+  mkdir -p CPAN/lib/voiceIt
+  cp voiceIt2.pm CPAN/lib/voiceIt
+  cd CPAN
+  h2xs -AX --skip-exporter --use-new-tests voiceIt::voiceIt2 -v $version
+  cd voiceIt-voiceIt2
+  perl Makefile.PL
+  make dist
+
+  cpan CPAN::Uploader
+  curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Perl/releases
+  echo "ls"
+  ls
+  cpan-upload --ignore-errors -u $PAUSEPERLUSERNAME -p $PAUSEPERLPASSWORD 'voiceIt-voiceIt2-'$version'.tar.gz'
 fi
